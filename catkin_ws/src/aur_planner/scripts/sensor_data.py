f72d5ba7caf5a48fcd597b5eb303ea19545eb70a
fjodBorg <fjordormaster@hotmail.com>
2021-05-06 10:50:49 +0200

    add comments

--- catkin_ws/src/aur_planner/scripts/sensor_data.py
+++ catkin_ws/src/aur_planner/scripts/sensor_data.py
@@ -12,23 +12,25 @@ class Sources():
         self._pos_array = np.array(pos_array)
 
     def inverse_euclidian_dist(self, dist_array):
+        # calculate euclidian distances for each source
         eucli_dist = np.array([np.linalg.norm(dist) for dist in dist_array])
         print("euclidian distances: " + str(eucli_dist))
+        # inverse it since it should be more present the closer it is
         inverse_eucli_dist = 1.0/eucli_dist
         print("inverse euclidian distances: " + str(inverse_eucli_dist))
+        # sum all the inversed euclidian distances 
         sum_inverse_dist = np.sum(inverse_eucli_dist)
         print("summed inverse distance: " + str(sum_inverse_dist))
-
         return sum_inverse_dist
 
     def get_relative_value(self, rob_pose):
         # does not take orientation into account
         # TODO take orientation into account
-        rob_pos = np.array([rob_pose.position.x, rob_pose.position.y, rob_pose.position.z])
         print("Source positions: \n" + str(self._pos_array))
+        rob_pos = np.array([rob_pose.position.x, rob_pose.position.y, rob_pose.position.z])
         print("Robot Position: \n" + str(rob_pos))
         dist_array = np.array([abs(rob_pos - pos) for pos in self._pos_array])
-        print("distance to each source: \n"+ str(dist_array))
+        print("distance to each source: \n" + str(dist_array))
         value = self.inverse_euclidian_dist(dist_array)
         print("\n")
 
@@ -37,33 +39,41 @@ class Sources():
 
 class Sub():
     def __init__(self, sources):
-        self.sources = sources
+        self.sources = sources  # gain access to source class
         self.listener()
         self.pose = None
 
+        # initialize publisher
+        self.pub = rospy.Publisher('sensor', Illuminance, queue_size=10)
+
+        # initialize sensor type
+        self.sensor = Illuminance()
+
     def listener(self):
+        # initialize subscriber
         rospy.Subscriber("pose", Pose, self.callback)
 
     def callback(self, pose):
+        # Do this when something is published to intilized topic
         self.pose = pose
         # rospy.loginfo(rospy.get_caller_id() + "I heard %s", pose.position)
-        self.sources.get_relative_value(pose)
+        # retrieve artifical value
+        self.sensor.illuminance = self.sources.get_relative_value(pose)
+        # publish the sensor msg
+        self.pub.publish(self.sensor)
 
 
 def main_loop():
-    pub = rospy.Publisher('sensor', Illuminance, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-    sensor = Illuminance()
-    sensor.illuminance = 0 
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         # sensor.illuminance += 0.1
         # rospy.loginfo(sensor.illuminance)
-        pub.publish(sensor)
         rate.sleep()
 
 
 if __name__ == '__main__':
     rospy.init_node('node_name', anonymous=True)
+    # init sources
     sources = Sources([[0, 2, 3], [3, 4, 5]])
     sub = Sub(sources)
     main_loop()
